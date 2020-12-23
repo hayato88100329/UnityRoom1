630bcd231b3cd21e786d4832bc95c0af0ba3e7b8
A601-02 <tnk.dat.class@gmail.com>
2020-12-23 13:41:56 +0900

    シーン入れ替え

---
+++ Assets/Scripts/ClickToNextScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToNextScene : MonoBehaviour
+{
+    [Tooltip("切り替えたいシーン名"), SerializeField]
+    string nextScene = "";
+
+    [Tooltip("シーン切り替え直後に次のシーンに切り替えないようにするにはチェック")]
+    public bool sceneChanged = false;
+
+    public void SetChangeFalse()
+    {
+        sceneChanged = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sceneChangedがtrueだったら、Updateはすぐ終了
+        if (sceneChanged) return;
+
+        var rank = SceneManager.GetSceneByName("Ranking");
+        if (rank.IsValid()) return;
+        //if (rank.IsValid() || !GameManager.CanChangeToTitle) return;
+
+        if (Input.GetButtonDown("Click"))
+        {
+            sceneChanged = true;
+            //TinyAudio.PlaySe(TinyAudio.Se.Click);
+            SceneManager.LoadScene(nextScene);
+        }
+
+
+    }
+
+}
